f21cc06b0543fb9549c2edf802db814f504f766b
Rishi Roshan D <drishiroshan@gmail.com>
2025-07-24 09:21:35 +0530

    assignment3

---
+++ C2TC_corejava_rishi/src/Assignment3/Main.java
@@ -0,0 +1,22 @@
+package Assignment3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Create Student object
+        Student s = new Student();
+
+        // Create Commission object using constructor
+        Commission emp = new Commission("Ashwin", "Pondy", "9150558521", 100000);
+
+        System.out.println("Name: " + emp.getName());
+        System.out.println("Address: " + emp.getAddress());
+        System.out.println("Phone: " + emp.getPhone());
+        System.out.println("Sales Amount: ₹" + emp.getSalesAmount());
+
+        emp.setSalesAmount(85000);
+        System.out.println("Updated Sales Amount: ₹" + emp.getSalesAmount());
+
+	}
+
+}
